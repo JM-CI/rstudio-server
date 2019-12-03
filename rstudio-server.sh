543a3fa1a55c24082c00a2d01d11a2affa11269e
root <root@clust1-head-2.cri.camres.org>
2019-12-03 15:43:38 +0000

    Added files

---
+++ rstudio-server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+catch() {
+	local JOBNO=$1
+	echo "Killing slurm job"
+	scancel $JOBNO
+	# Necessary to bury the output as this seems to get exectued more than
+	# once on a sigint for example...
+	rm "$HOME"/rstudio-"$JOBNO".out > /dev/null 2>&1
+	exit
+}
+
+ARGS="$@"
+echo "Starting slurm job"
+RES=$(sbatch $ARGS /home/software/utilities/rstudio-server/rstudio-server.sbatch)
+JOBNO=${RES##* }
+echo "Job number: $JOBNO"
+echo "Press ^C or close terminal to cancel job"
+trap "catch $JOBNO" ERR EXIT SIGINT SIGTERM KILL
+sleep 5
+tail -f "$HOME"/rstudio-"$JOBNO".out
